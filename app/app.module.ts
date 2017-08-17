ad6303f5b1dccb2c33e3e1f7df32e3c1bc8eed87
George Batalinski <george.batalinski@gmail.com>
2017-08-17 10:51:23 -0400

    setup exports

---
+++ app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FunAppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    FunAppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [FunAppComponent]
+})
+export class FunAppModule { }
